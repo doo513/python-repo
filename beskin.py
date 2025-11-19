fb2bb95181c6a11f892a218757681efa3ec14ed1
doo513 <doo3370@outlook.com>
2025-11-19 13:18:02 +0900

    아이스크림

--- beskin.py
+++ beskin.py
@@ -11,7 +11,10 @@ win = []
 for i in range(min):
     win.append(int((target - i+1) % block))
 
-for i in range(int(target / (min+max))+1):
-    sum += i*(min+max)
-    if(sum < target): 
-        print(sum)
+for i in range(1,int(target / (min+max))+1):
+    sum = min + i*(min+max) + 1
+    if(min <= (target - i+1) % block or (target - i+1) % block <= max):
+        if(sum <= target):
+            print(sum)
+    else:
+        print(-1)
