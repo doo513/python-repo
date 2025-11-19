68ce361a330411510bf0db9f050eaa46be754a80
doo513 <doo3370@outlook.com>
2025-11-19 13:58:19 +0900

    abc

--- beskin.py
+++ beskin.py
@@ -6,15 +6,12 @@ sum = min
 
 block = min + max
 
-win = []
-
-for i in range(min):
-    win.append(int((target - i+1) % block))
-
 for i in range(1,int(target / (min+max))+1):
-    sum = min + i*(min+max) + 1
-    if(min <= (target - i+1) % block or (target - i+1) % block <= max):
-        if(sum <= target):
-            print(sum)
-    else:
+    if min > (target - min) % block or (target - min) % block > max:
         print(-1)
+        break
+    else:
+        sum = min + i*(min+max) + 1
+        if(sum < target):
+            print(sum, end = ' \n')
+    
